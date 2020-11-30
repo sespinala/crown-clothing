55c82d4d4a49102679b965aa52ec941a2fc99ead
Sara Espinal <sara.espinala@perficient.com>
2020-11-30 12:12:43 -0500

    added checkout coomponent, types constant and redirect to login page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/original.svg';
 
@@ -20,15 +22,16 @@ const Header = ({ currentUser }) => (
       <Link className='option' to='/contact'>
         CONTACT
       </Link>
-      {
-        currentUser ? 
+      { currentUser ? (
         <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-        :
+      ) : (
         <Link className='option' to='/signIn' >
           SIGN IN
         </Link>
-      }
+      )}
+      <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
